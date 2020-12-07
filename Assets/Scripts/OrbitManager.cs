bdc151ce457fe62e1c8404f312516046a6701a57
LongJonJon <46792496+LongJonJon@users.noreply.github.com>
2020-12-07 00:59:31 -0700

    Planet Revolution V1

---
+++ Assets/Scripts/OrbitManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitManager : MonoBehaviour {
+    public GameObject SolarSystem;
+    public float time = 0;
+    OrbitingBody[] solarSystemObjects;
+    private OrbitingBody focus;
+    Vector3 priorloc = new Vector3 ();
+    public float focus_Delta = 3;
+    protected float _focusDelta;
+    float focusDelta {
+        get {
+            return _focusDelta;
+        }
+        set {
+            if (_focusDelta > focus_Delta) _focusDelta = focus_Delta;
+            else _focusDelta += value;
+        }
+    }
+
+    void Start () {
+        solarSystemObjects = GameObject.FindObjectsOfType<OrbitingBody> ();
+        foreach (OrbitingBody o in solarSystemObjects) {
+            if (o.type == OrbitingBody.Significance.Sun) {
+                priorloc = o.transform.position;
+                focus = o;
+                break;
+            }
+        }
+    }
+
+    void LateUpdate () {
+        time += Time.deltaTime;
+        foreach (OrbitingBody o in solarSystemObjects) {
+            if (o == focus)
+                focus.transform.position = Vector3.Lerp (priorloc, SolarSystem.transform.position, _focusDelta);
+            else
+                o.UpdatePos (time);
+        }
+        focusDelta = Time.deltaTime;
+    }
+
+    public void UpdateFocus (GameObject g) {
+        Debug.Log(g.name);
+        focus = g.GetComponent<OrbitingBody> ();
+        if (!focus) return;
+
+        _focusDelta = 0;
+        priorloc = g.transform.position;
+
+        foreach (OrbitingBody o in solarSystemObjects)
+            o.transform.SetParent (SolarSystem.transform, true);
+            
+        foreach (OrbitingBody o in solarSystemObjects)
+            o.ResetOrbit ();
+
+        focus.ReflectOrbit ();
+    }
+}
---
+++ Assets/Scripts/OrbitingBody.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class OrbitingBody : MonoBehaviour {
+    public enum Significance { Planet = 1, Moon = 2, Sun = 0 }
+    public Significance type;
+    public OrbitingBody originalOrbiting;
+    public OrbitingBody orbiting;
+    float distance;
+    public float frequency;
+
+    void Start () {
+        OrbitingBody[] options = GetComponentsInParent<OrbitingBody> ();
+        OrbitingBody parent = null;
+        foreach (OrbitingBody p in options) {
+            if (this.type > p.type) {
+                parent = p;
+                break;
+            }
+        }
+        if (!parent) return;
+
+        originalOrbiting = orbiting = parent;
+        distance = Vector3.Distance (this.transform.position, parent.transform.position);
+    }
+
+    public void ResetOrbit () {
+        if (type == Significance.Sun)
+            this.transform.SetParent (GameObject.FindObjectOfType<OrbitManager> ().SolarSystem.transform, true);
+        else
+            this.transform.SetParent (originalOrbiting.transform, true);
+        orbiting = originalOrbiting;
+
+    }
+
+    public void ReflectOrbit (OrbitingBody o = null) {
+        if (!o) {
+            this.transform.SetParent (GameObject.FindObjectOfType<OrbitManager> ().SolarSystem.transform, true);
+        } else {
+            this.transform.SetParent (o.transform, true);
+        }
+        orbiting = o;
+
+        if (originalOrbiting == null) return;
+        originalOrbiting.ReflectOrbit (this);
+    }
+
+    public void UpdatePos (float time) {
+        if (!orbiting) return;
+        if (orbiting != originalOrbiting)
+            Debug.DrawLine (this.transform.position, orbiting.transform.position, new Color (0, 255, 150));
+        else
+            Debug.DrawLine (this.transform.position, orbiting.transform.position, new Color (150, 255, 0));
+
+        float x = 2 * Mathf.PI * time / ((originalOrbiting != orbiting) ? orbiting.frequency : frequency) + ((originalOrbiting != orbiting) ? Mathf.PI : 0);
+        Vector3 newpos = new Vector3 (Mathf.Sin (x), 0, Mathf.Cos (x));
+        this.transform.localPosition = newpos * ((originalOrbiting != orbiting) ? orbiting.distance : distance);
+    }
+}
